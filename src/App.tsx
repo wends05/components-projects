b0afb9ae9ae2e7fd321aca602e96c617ad3fbfbf
wends05 <cxnner05@gmail.com>
2025-02-08 16:34:16 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Component Design:
+      </h1>
+      <h2>
+        Assignment 1
+      </h2>
+    </>
+  )
+}
+
+export default App
