03e3b586a9f8e65c00c8d7740c1c37dd7b04e018
wends05 <cxnner05@gmail.com>
2025-02-09 13:31:14 +0800

    main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-
-import './App.css'
+import Card from "./components/Card";
 
 function App() {
-
   return (
-    <>
-      <h1>
-        Component Design:
-      </h1>
-      <h2>
-        Assignment 1
-      </h2>
-    </>
-  )
+    <div className="flex flex-col gap-10 h-screen p-10">
+      <div>
+        <h1>Component Design</h1>
+        <h2>Assignment 1</h2>
+        <h3>Wendell Terence Dador</h3>
+      </div>
+      <h2 className="flex justify-center w-full">Component: Card</h2>
+
+      <div className="flex flex-col md:flex-row items-center justify-center gap-10 p-10">
+        <Card
+          title="Large Card"
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta cumque vero sed, possimus placeat veniam, temporibus velit atque similique expedita sequi. Temporibus, dolore?"
+          size="large"
+        />
+        <Card
+          title="Small Card"
+          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta cumque vero sed, possimus placeat veniam, temporibus velit atque similique expedita sequi. Temporibus, dolore?"
+          size="small"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
